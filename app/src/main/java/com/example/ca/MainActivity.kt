a181455f1e3e0bbd1da03688ee68e8888e7ccdb8
13yashkundu <yshkundu@gmail.com>
2026-05-23 23:38:29 +0530

    Buttons logic added

--- app/src/main/java/com/example/ca/MainActivity.kt
+++ app/src/main/java/com/example/ca/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ca
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val inputBox = findViewById<EditText>(R.id.input_box)
+        val digitTotal = findViewById<TextView>(R.id.digit_total)
+    }
+    fun addNumber(){
+
     }
 }
